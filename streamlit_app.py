ce7421a723f171964d4e2fdaa4cb8d8ded164bcf
KARAM022 <126792233+KARAM022@users.noreply.github.com>
2025-04-13 11:06:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,22 @@
 import streamlit as st
+import pandas as pd
 
-st.title("My Streamlit App")
-st.write("Hello, this is my Python website built with Streamlit!")
+
+
+import streamlit as st
+import pandas as pd
+
+def show_sheets_page():
+    st.title("Lecture de Google Sheets dans Streamlit")
+
+    # Remplace cet ID par celui de ta feuille Google Sheets
+    sheet_id = "14GC4RUwAcezwnWs2gjDtPMpSoxzFHH9Us3fts9-cdFU"
+    sheet_name = "str1"
+    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+
+    try:
+        df = pd.read_csv(url)
+        st.success("Données chargées avec succès depuis Google Sheets !")
+        st.dataframe(df)
+    except Exception as e:
+        st.error(f"Erreur lors du chargement des données : {e}")
